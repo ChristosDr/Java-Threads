01c28e7c8948690648bbeb2b3a1fd40d2ebbbfe6
ChristosDr <117837415+ChristosDr@users.noreply.github.com>
2024-04-09 22:23:03 +0300

    Added Threads using Sync(locking) classes

---
+++ Ergasia 4/SharedCounterArrayGlobal_Sync.java
@@ -0,0 +1,69 @@
+public class SharedCounterArrayGlobal_Sync {
+
+    public static void main(String[] args) {
+        int end = 1000;
+        int[] array = new int[end];
+        int numThreads = 4;
+
+		SharedArray sharedArray = new SharedArray(array);
+
+        CounterThread threads[] = new CounterThread[numThreads];
+
+        for (int i = 0; i < numThreads; i++) {
+            threads[i] = new CounterThread(sharedArray, end);
+            threads[i].start();
+        }
+
+        for (int i = 0; i < numThreads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        check_array(array, end, numThreads);
+    }
+
+    static void check_array(int[] array, int end, int numThreads) {
+        int errors = 0;
+
+        System.out.println("Checking...");
+
+        for (int i = 0; i < end; i++) {
+            if (array[i] != numThreads * i) {
+                errors++;
+                System.out.printf("%d: %d should be %d\n", i, array[i], numThreads * i);
+            }
+        }
+        System.out.println(errors + " errors.");
+    }
+
+	static class SharedArray {
+		private int[] array;
+
+		public SharedArray(int[] array){
+			this.array = array;
+		}
+		public synchronized void incArr(int index){
+			for(int j=0; j<index; j++){
+				array[index]++;
+			}
+		}
+	}
+
+    static class CounterThread extends Thread {
+        private SharedArray sharedArray;
+        private int end;
+
+        public CounterThread(SharedArray sharedArray, int end) {
+            this.sharedArray = sharedArray;
+            this.end = end;
+        }
+
+        public void run() {
+            for (int i = 0; i < end; i++) {
+                sharedArray.incArr(i);
+            }
+        }
+    }
+}
